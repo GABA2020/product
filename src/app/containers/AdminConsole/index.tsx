8a9fd76a82b2572b0c3e34c97b7c4350370dd9ed
William B <williamblacknall@gmail.com>
2020-09-05 12:22:14 -0400

    Payments and the admin console have been put back in Auth
    Successfully retrieved user email state too, so that's nice.

--- src/app/containers/AdminConsole/index.tsx
+++ src/app/containers/AdminConsole/index.tsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
 import useFirestore from '../../../hooks/useFirestore';
 import { AdminVerifyProfileModal } from '../../components/Modal/AdminVerifyProfileModal';
+import { useSelector, useDispatch } from 'react-redux';
+import { authSelector } from 'redux/Auth/selectors';
 
 export const AdminConsole = () => {
   const [selectedImg, setSelectedImg] = useState(null);
   const [name, setName] = useState(null);
-  const [email, setEmail] = useState(null);
+  const [emails, setEmail] = useState(null);
 
   const { docs } = useFirestore('member_data') as any;
   console.log('docs', docs);
+
+  const { email } = useSelector(authSelector);
+
+  const clickHandler = () => {
+    console.log(email);
+  };
   return (
     <>
+      <button className="btn btn-success" onClick={clickHandler}>
+        Testing Email
+      </button>
       {docs &&
         docs.map(doc => (
           <div className="tile is-child box" key={doc.id}>
@@ -38,7 +49,7 @@ export const AdminConsole = () => {
           selectedImg={selectedImg}
           setSelectedImg={setSelectedImg}
           name={name}
-          email={email}
+          email={emails}
         />
       )}
     </>

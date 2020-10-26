9027591e66bcd43ac9122c7c9baf195791cfc763
Luis Gonzalez <luisjgh1@gmail.com>
2020-10-26 13:27:26 -0500

    Filter by exam and number of stars

--- src/app/product/components/ReviewsSection/index.tsx
+++ src/app/product/components/ReviewsSection/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Review from './Review'
 
 
-const reviews = [{
+const REVIEWS = [{
 	numberOfStars: 3,
 	title: 'awesome resource, but you’ll be paying a pretty penny',
 	comment: `
@@ -13,7 +13,8 @@ const reviews = [{
 		to get a 250 or above so I spared no expense.
 	`,
 	date: 'May 15, 2020',
-	messageType: 'Step One',
+  messageType: 'Step One',
+  exam: '1',
 	user: '@anotherdoc',
 	id: '1234'
 },
@@ -28,7 +29,8 @@ const reviews = [{
 		to get a 250 or above so I spared no expense.
 	`,
 	date: 'May 15, 2020',
-	messageType: 'Step One',
+  messageType: 'Step Two',
+  exam: '2',
 	user: '@anotherdoc',
 	id: '1234'
 },
@@ -43,265 +45,178 @@ const reviews = [{
 		to get a 250 or above so I spared no expense.
 	`,
 	date: 'May 15, 2020',
-	messageType: 'Step One',
+  messageType: 'Step One',
+  exam: '1',
 	user: '@anotherdoc',
 	id: '1234'
 }]
 
-const ReviewSection = () => (
-	<section className="section-review">
-		<div className="container">
-			<div className="review-layout">
-				<div className="review-slidebar">
-					<div className="portlet-review">
-						<div className="review-heading">
-							Reviews 2
-              <h2 className="review-judge-title">Reviews</h2>
-							<div className="judge-sum">
-								<p>4.5 out of 5 stars</p>
-							</div>
-						</div>
-						{/*end judge-sum*/}
-						<div className="judge-category">
-							<div className="judge-item">
-								<div className="judge-num-star">
-									<span className="judge-num">5</span>
-									<span className="judge-star active">&nbsp;</span>
-								</div>
-								{/*end judge-num-star*/}
-								<div className="judge-progress">
-									<div className="progress">
-										<div
-											className="progress-bar"
-											role="progressbar"
-											aria-valuenow={85}
-											aria-valuemin={0}
-											aria-valuemax={100}
-											style={{ width: '85%' }}
-										/>
-									</div>
-								</div>
-								{/*end judge-progress*/}
-								<div className="judge-percent">
-									<span>85%</span>
-								</div>
-								{/*end judge-percent*/}
-							</div>
-							{/*end judge-item*/}
-							<div className="judge-item">
-								<div className="judge-num-star">
-									<span className="judge-num">4</span>
-									<span className="judge-star active">&nbsp;</span>
-								</div>
-								{/*end judge-num-star*/}
-								<div className="judge-progress">
-									<div className="progress">
-										<div
-											className="progress-bar"
-											role="progressbar"
-											aria-valuenow={60}
-											aria-valuemin={0}
-											aria-valuemax={100}
-											style={{ width: '9%' }}
-										/>
-									</div>
-								</div>
-								{/*end judge-progress*/}
-								<div className="judge-percent">
-									<span>9%</span>
-								</div>
-								{/*end judge-percent*/}
-							</div>
-							{/*end judge-item*/}
-							<div className="judge-item">
-								<div className="judge-num-star">
-									<span className="judge-num">3</span>
-									<span className="judge-star active">&nbsp;</span>
-								</div>
-								{/*end judge-num-star*/}
-								<div className="judge-progress">
-									<div className="progress">
-										<div
-											className="progress-bar"
-											role="progressbar"
-											aria-valuenow={60}
-											aria-valuemin={0}
-											aria-valuemax={100}
-											style={{ width: '4%' }}
-										/>
-									</div>
-								</div>
-								{/*end judge-progress*/}
-								<div className="judge-percent">
-									<span>4%</span>
-								</div>
-								{/*end judge-percent*/}
-							</div>
-							{/*end judge-item*/}
-							<div className="judge-item">
-								<div className="judge-num-star">
-									<span className="judge-num">2</span>
-									<span className="judge-star active">&nbsp;</span>
-								</div>
-								{/*end judge-num-star*/}
-								<div className="judge-progress">
-									<div className="progress">
-										<div
-											className="progress-bar"
-											role="progressbar"
-											aria-valuenow={1}
-											aria-valuemin={0}
-											aria-valuemax={100}
-											style={{ width: '2%' }}
-										/>
-									</div>
-								</div>
-								{/*end judge-progress*/}
-								<div className="judge-percent">
-									<span>1%</span>
-								</div>
-								{/*end judge-percent*/}
-							</div>
-							{/*end judge-item*/}
-							<div className="judge-item">
-								<div className="judge-num-star">
-									<span className="judge-num">1</span>
-									<span className="judge-star active">&nbsp;</span>
-								</div>
-								{/*end judge-num-star*/}
-								<div className="judge-progress">
-									<div className="progress">
-										<div
-											className="progress-bar"
-											role="progressbar"
-											aria-valuenow={1}
-											aria-valuemin={0}
-											aria-valuemax={100}
-											style={{ width: '1%' }}
-										/>
-									</div>
-								</div>
-								{/*end judge-progress*/}
-								<div className="judge-percent">
-									<span>1%</span>
-								</div>
-								{/*end judge-percent*/}
-							</div>
-							{/*end judge-item*/}
-						</div>
-						{/*end judge-category*/}
-					</div>
-					{/*end portlet-review*/}
-					<div className="portlet-mention">
-						<h3 className="mention-title">See reviews that mention</h3>
-						<ul className="mention-list">
-							<li>
-								<a href="#" className="btn-mention">
-									scores improved
+const judgeReviews = [{
+  value: 85,
+  stars: 5
+},{
+  value: 9,
+  stars: 4
+},{
+  value: 4,
+  stars: 3
+},{
+  value: 1,
+  stars: 2
+},{
+  value: 1,
+  stars: 1
+}]
+
+
+const ReviewSection = () => {
+  let [reviews, setReviews] = useState(REVIEWS);
+  const handleFilterReviews =  (property, value) => setReviews(
+    REVIEWS.filter(rev => rev[property] === value)
+  )
+
+  return (
+    <section className="section-review">
+      <div className="container">
+        <div className="review-layout">
+          <div className="review-slidebar">
+            <div className="portlet-review">
+              <div className="review-heading">
+                Reviews 2
+                <h2 className="review-judge-title">Reviews</h2>
+                <div className="judge-sum">
+                  <p>4.5 out of 5 stars</p>
+                </div>
+              </div>
+              <div className="judge-category">
+                {
+                  judgeReviews.map(rev => (
+                    <div className="judge-item">
+                      <div className="judge-num-star">
+                        <span className="judge-num">{rev.stars}</span>
+                        <span className="judge-star active" onClick={() => handleFilterReviews('numberOfStars', rev.stars)}>&nbsp;</span>
+                      </div>
+                      <div className="judge-progress">
+                        <div className="progress">
+                          <div
+                            className="progress-bar"
+                            role="progressbar"
+                            aria-valuenow={rev.value}
+                            aria-valuemin={0}
+                            aria-valuemax={100}
+                            style={{ width: `${rev.value}%` }}
+                          />
+                        </div>
+                      </div>
+                      <div className="judge-percent">
+                        <span>{rev.value}%</span>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+            <div className="portlet-mention">
+              <h3 className="mention-title">See reviews that mention</h3>
+              <ul className="mention-list">
+                <li>
+                  <a href="#" className="btn-mention">
+                    scores improved
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="btn-mention">
+                    boost
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="btn-mention">
+                    cost
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="btn-mention">
+                    schedule
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="btn-mention">
+                    lectures
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="btn-mention">
+                    update
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="btn-mention">
+                    unique style
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="btn-mention">
+                    really like
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="btn-mention">
+                    price
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="portlet-filter">
+              <h3 className="mention-title">Filter by exam</h3>
+              <div className="select-box">
+                <select className="form-control" onChange={({ target: { value }}) => handleFilterReviews('exam', value)}>
+                  <option value={0}>Select</option>
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                </select>
+              </div>
+            </div>
+            <div className="portlet-filter">
+              <h3 className="mention-title">Filter by discipline</h3>
+              <div className="select-box">
+                <select className="form-control">
+                  <option value={0}>Select</option>
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                </select>
+              </div>
+            </div>
+            <div className="portlet-filter">
+              <h3 className="mention-title">Review this resource</h3>
+              <div className="mention-insight">
+                <p>Help other students with your insight.</p>
+              </div>
+              <div className="write-button">
+                <a href="#" className="btn btn-write-review">
+                  Write a Review
                 </a>
-							</li>
-							<li>
-								<a href="#" className="btn-mention">
-									boost
+              </div>
+            </div>
+          </div>
+          <div className="review-main">
+            <div className="review-content">
+              <div className="portlet-message">
+                {
+                  reviews.map(rev => (
+                    <Review {...rev}/>
+                  ))
+                }
+              </div>
+              <div className="load-more">
+                <a href="#" className="btn btn-loadmore">
+                  Load More Reviews
                 </a>
-							</li>
-							<li>
-								<a href="#" className="btn-mention">
-									cost
-                </a>
-							</li>
-							<li>
-								<a href="#" className="btn-mention">
-									schedule
-                </a>
-							</li>
-							<li>
-								<a href="#" className="btn-mention">
-									lectures
-                </a>
-							</li>
-							<li>
-								<a href="#" className="btn-mention">
-									update
-                </a>
-							</li>
-							<li>
-								<a href="#" className="btn-mention">
-									unique style
-                </a>
-							</li>
-							<li>
-								<a href="#" className="btn-mention">
-									really like
-                </a>
-							</li>
-							<li>
-								<a href="#" className="btn-mention">
-									price
-                </a>
-							</li>
-						</ul>
-					</div>
-					{/*end portlet-mention*/}
-					<div className="portlet-filter">
-						<h3 className="mention-title">Filter by exam</h3>
-						<div className="select-box">
-							<select className="form-control">
-								<option value={0}>Select</option>
-								<option value={1}>1</option>
-								<option value={2}>2</option>
-							</select>
-						</div>
-					</div>
-					{/*end portlet-filter*/}
-					<div className="portlet-filter">
-						<h3 className="mention-title">Filter by discipline</h3>
-						<div className="select-box">
-							<select className="form-control">
-								<option value={0}>Select</option>
-								<option value={1}>1</option>
-								<option value={2}>2</option>
-							</select>
-						</div>
-					</div>
-					{/*end portlet-filter*/}
-					<div className="portlet-filter">
-						<h3 className="mention-title">Review this resource</h3>
-						<div className="mention-insight">
-							<p>Help other students with your insight.</p>
-						</div>
-						<div className="write-button">
-							<a href="#" className="btn btn-write-review">
-								Write a Review
-              </a>
-						</div>
-					</div>
-					{/*end portlet-filter*/}
-				</div>
-				{/*end review-slidebar*/}
-				<div className="review-main">
-					<div className="review-content">
-						<div className="portlet-message">
-							{
-								reviews.map(rev => (
-									<Review {...rev}/>
-								))
-							}
-						</div>
-						{/*end portlet-message*/}
-						<div className="load-more">
-							<a href="#" className="btn btn-loadmore">
-								Load More Reviews
-              </a>
-						</div>
-					</div>
-					{/*end review-content*/}
-				</div>
-				{/*end review-main*/}
-			</div>
-			{/*end review-layout*/}
-		</div>
-	</section>
-);
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+)};
 
 export default ReviewSection;

9dc36b40e42ff19c7baded63d6ae7b28f7762e67
Luis Gonzalez <luisjgh1@gmail.com>
2020-11-02 17:55:00 -0500

    Restored GQL client url

--- src/helpers/graphqlClient.ts
+++ src/helpers/graphqlClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const link = new HttpLink({
-  uri: 'https://us-central1-august-water-280101.cloudfunctions.net/resourceApi',
+  uri: 'https://content-mackerel-67.hasura.app/v1/graphql',
 });
 const cache = new InMemoryCache();
 const graphQLClient = new ApolloClient({

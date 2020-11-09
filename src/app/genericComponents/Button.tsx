a4fc37164f5470a39fd376ff84325059e092811f
Luis Gonzalez <luisjgh1@gmail.com>
2020-11-09 01:52:28 -0500

    Frontend Tweaks

--- src/app/genericComponents/Button.tsx
+++ src/app/genericComponents/Button.tsx
@@ -1,36 +1,22 @@
-import React from 'react';
+import { ButtonProps } from 'antd/lib/button';
 import styled from 'styled-components';
 
-interface GenericButtonProps {
-  children: any;
-  additionalStyles: String;
-  color?: string;
-  backgroundColor?: string
+type Props = {
+  color?: 'string';
+  backgroundColor?: 'string';
 }
 
-const Button = styled.button`
+const Button = styled.button<Props>`
   font-size: 1.6rem;
   font-weight: 500;
   padding: 9px 14px;
-  line-height: 1.1;
-  color: ${(props: any) => props.color || props.theme.color.darkBlue};
-  background-color: ${(props: any) => props.backgroundColor || props.theme.color.softPurple};
-  border-radius: 2px;
+  color: ${props => props.color || props.theme.color.darkBlue};
+  background-color: ${props => props.backgroundColor || props.theme.color.softPurple};
+  border-radius: 5px;
   margin-top: -20px;
   outline: none;
   border: none;
   margin-top: 5px;
-
-  ${(props: any) => props.additionalStyles}
 `;
 
-const GenericButton = (props: GenericButtonProps) => {
-  const { children } = props;
-  return (
-    <Button {...props}>
-      {children}
-    </Button>
-  )
-};
-
-export default GenericButton;
+export default Button;

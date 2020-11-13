4ee64585598b9974a488175ff60455fc1c098382
Sergio Munoz <snmunoz@gmail.com>
2020-11-13 02:16:26 -0500

    people

--- src/service/queries/index.ts
+++ src/service/queries/index.ts
@@ -31,7 +31,14 @@ export const RESOURCE_DETAIL = gql`
       tags
     }
   }
-`
+`;
+export const CONNECTED_USERS = gql`
+  query ConnectedUsers($email: String!) {
+    connectedUsers(email: $email) {
+      email
+    }
+  }
+`;
 
 export const USERS_QUERY = gql`
   query Users {
@@ -39,23 +46,25 @@ export const USERS_QUERY = gql`
       creationDate
       degrees
       email
-      gender
-      last_login
       medicalSchool
       name
       student_status
       username
       year_in_program
+      school_year
+      specialties
+      mcat
+      step_1
+      step_2
+      step_3
     }
   }
-`
+`;
 
 export const GET_LOCKER = gql`
   query getResources($email: String!) {
-    resources_locker(where: {
-      user_id:{_eq: $email }
-    }) {
+    resources_locker(where: { user_id: { _eq: $email } }) {
       resource_id
     }
   }
-`
+`;

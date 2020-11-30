804617cad24e4477d43e46a8f6937cd14f1b4aab
Sergio Munoz <snmunoz@gmail.com>
2020-11-30 13:18:59 -0500

    schools and aplicantdb header

--- src/app/aplicantDatabase/index.js
+++ src/app/aplicantDatabase/index.js
@@ -1,4 +1,52 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import Button from '../genericComponents/Button';
+import { Grid } from 'semantic-ui-react';
+
+const BlueHero = require('../../assets/images/sprites/blueHero.jpg');
+const HeaderContainer = styled.div`
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  background: url('${BlueHero}');
+  background-size: 100% 250px;
+  background-repeat: repeat-x;
+  background-color: white;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-self: center;
+  width: 100%;
+  height: 150px;
+  color: white;
+
+  ${props => props.theme.rules.narrowWidth}
+`;
+
+const TutorialButton = styled(Button)`
+  background-color: #eeaa35;
+  color: ${props => props.theme.color.goodNightColor};
+  padding: 15px 14px 14px;
+  border-radius: 6px;
+  margin: 30px;
+  font-size: 16px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: 0.1px;
+  text-align: center;
+  color: $goodNightColor;
+  min-width: 120px;
+`;
+
+const Title = styled.div`
+  margin: 30px;
+  width: 800px;
+`;
 
 //codesandbox.io/s/react-iframe-demo-g3vst codePen =
 function Iframe(props) {
@@ -11,8 +59,27 @@ function Iframe(props) {
 
 const AplicantDatabase = () => {
   return (
-    <div className="iframe">
-      <h3>Aplicant Database</h3>
+    <div>
+      <HeaderContainer>
+        <ContentContainer>
+          <Grid.Column>
+            <Title>
+              <h3>Applicant Database</h3>
+              <p>
+                {' '}
+                Whether its to keep a competitive edge or to stay on track, we
+                know how important it is to see what others in your field have
+                accomplished. Our growing database contains over 5 years of data
+                and +5000 data points from former applicants to help you
+                strategize for success!
+              </p>
+            </Title>
+          </Grid.Column>
+          <Grid.Column>
+            <TutorialButton>Tutorial Video</TutorialButton>
+          </Grid.Column>
+        </ContentContainer>
+      </HeaderContainer>
       <Iframe
         iframe={
           '<iframe class="airtable-embed" src="https://airtable.com/embed/shrKweFzBEkoWqzTp?backgroundColor=blue&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="1400px" style="background: transparent; border: 1px solid #ccc;"></iframe>'
